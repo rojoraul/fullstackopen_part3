5ac27bf159aef884d21a70bdeabbf4db19096710
Raúl Rojo <raulrojo98@gmail.com>
2022-12-25 20:07:20 +0100

    Ejercicio 3.7 & 3.8

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    if (tokens.method(req, res) === 'POST') {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            JSON.stringify(req.body)
+        ].join(' ')
+    }
+}))
 
 let persons = [
     {
