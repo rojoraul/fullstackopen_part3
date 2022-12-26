6985425e36c72ebed675edd06632cb6ec992df78
Raúl Rojo <raulrojo98@gmail.com>
2022-12-26 01:35:29 +0100

    Exercise 3.19, 3.20, 3.21 and 3.22

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 module.exports = (err, request, response, next) => {
-  console.error(err)
-  if (err.name === 'CastError') { response.status(400).send({ error: 'id is malformed' }) } else { response.status(500).end() }
-}
+	console.error(err);
+	if (err.name === 'CastError') { response.status(400).send({ error: 'id is malformed' }); } else { response.status(500).send({error: err.message}); }
+	next();
+};
